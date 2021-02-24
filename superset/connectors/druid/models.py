576b36854276ee690bf8f15cc4b8c48b5e10c52f
Arpith <arpikp@gmail.com>
2021-02-24 10:56:54 +0530

    add ssl

--- superset/connectors/druid/models.py
+++ superset/connectors/druid/models.py
@@ -24,6 +24,9 @@ from datetime import datetime, timedelta
 from distutils.version import LooseVersion
 from multiprocessing.pool import ThreadPool
 from typing import Any, cast, Dict, Iterable, List, Optional, Set, Tuple, Union
+import ssl
+import requests
+
 
 import pandas as pd
 import sqlalchemy as sa
@@ -58,6 +61,29 @@ from superset.models.helpers import AuditMixinNullable, ImportExportMixin, Query
 from superset.typing import FilterValues, Granularity, Metric, QueryObjectDict
 from superset.utils import core as utils
 from superset.utils.date_parser import parse_human_datetime, parse_human_timedelta
+
+
+try:
+    assert requests.__version__ != "2.18.0"
+    import requests.packages.urllib3.util.ssl_ as ssl_
+    import requests.packages.urllib3.connection as connection
+except (ImportError,AssertionError,AttributeError):
+    import urllib3.util.ssl_ as ssl_
+    import urllib3.connection as connection
+
+ssl._create_default_https_context = ssl._create_unverified_context
+def _ssl_wrap_socket(sock, keyfile=None, certfile=None, cert_reqs=None,
+                     ca_certs=None, server_hostname=None,
+                     ssl_version=None, ciphers=None, ssl_context=None,
+                     ca_cert_dir=None, **kwargs):
+    return ssl_.ssl_wrap_socket(sock, keyfile=keyfile, certfile=certfile,
+                      cert_reqs=cert_reqs, ca_certs=ca_certs,
+                      server_hostname=server_hostname, ssl_version=ssl_version,
+                      ciphers=ciphers, ssl_context=ssl_context,
+                      ca_cert_dir=ca_cert_dir)
+
+connection.ssl_wrap_socket = _ssl_wrap_socket
+
 
 try:
     import requests
